babd3127a9ef8f630df05b88a9368551e90759a7
IHateC0ward <937648864@qq.com>
2026-08-01 16:23:36 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  ...(isGitHubPages
-    ? {
-       
-      }
-    : {}),
 };
 
 export default nextConfig;
