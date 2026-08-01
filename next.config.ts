9dd1c7ee39ce39339d784cfd2b16c7d49485218c
IHateC0ward <937648864@qq.com>
2026-08-01 17:03:54 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/sansu-souzoukan-ready-for-github-pages",
+  assetPrefix: "/sansu-souzoukan-ready-for-github-pages/",
   trailingSlash: true,
   images: {
     unoptimized: true,
